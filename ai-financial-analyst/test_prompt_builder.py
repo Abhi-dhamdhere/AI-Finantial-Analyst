57bdc4ed4cf1acf53b2bb00959bb808bfb15c8a1
Abhishek Dhamdhere <110092108+Abhi-dhamdhere@users.noreply.github.com>
2026-04-03 15:40:36 +0530

    Delete ai-financial-analyst/test_prompt_builder.py

--- ai-financial-analyst/test_prompt_builder.py
+++
@@ -1,9 +0,0 @@
-from pdf_parser import extract_text_from_pdf
-from prompt_builder import build_prompt
-
-file_path = "C:/Users/dhamd/Desktop/Project/BharatFin AI/ai-financial-analyst/HDFCQ2.pdf"
-
-text = extract_text_from_pdf(file_path)
-prompt = build_prompt(text)
-
-print(prompt[:2000])  # preview prompt
